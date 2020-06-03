3f95f254e8f64b443c4243ad0a580e9902534085
David Williams <davidrwilliams87@gmail.com>
2020-06-03 19:06:16 +0100

    Updating the land demand scripts to use regional data, creating a cleaned regional LCA dataset (so the names match GENuS) and recalculating the country-level land demand.

--- Scripts/LandUseCalcs_v1.R
+++ Scripts/LandUseCalcs_v1.R
@@ -1,14 +1,29 @@
+# Read me ----
+# Taking the LCA data from Poore & Nemecek (provided by Mike Clark) and linking it to estimates
+# of wild meat consumption in each country
+#
+# This version uses region-specific LCAs
+#
 # Packages -----
 library(tidyverse)
 library(dplyr)
 library(tidyr)
+library(countrycode)
 
 options(tibble.width = Inf)
 rm(list=ls())
 
 # Load LCA and GENus data -----
-lca <- read_csv("Data/LCAdataforbushmeat.csv")
 protein <- read_csv("ProcessedData/GENusData_cleaned.csv")
+
+# First time: clean up lca data to match protein names
+# lca <- read_csv("Data/Region Land Estimates.csv")
+# lca <- lca %>%
+#   mutate(Product = case_when(Food.Group == "Bovine Meat (Beef Herd)" ~ "Bovine Meat (beef herd)",
+#                              Food.Group == "Mutton & Goat Meat" ~ "Lamb & Mutton",
+#                              TRUE ~ Food.Group))
+# write_csv(x = lca, path = "ProcessedData/RegionalLCA_cleaned.csv")
+lca <- read_csv("ProcessedData/RegionalLCA_cleaned.csv")
 
 # 1) Calculate proption of different meats in each country ----
 types <- names(protein)
@@ -100,21 +115,92 @@ land_grab <- protein %>%
   filter(!is.na(gamepcpa)) %>%
   mutate(kg_extra = Proportion * gamepcpa)
 
+# Get the region for each country -----
+land_grab <- land_grab %>%
+  mutate(Region = countrycode(ISO3, origin = "iso3c", destination = "region23"))
+
+# # Which regions for missing data? [First time only] ----
+# # Which regions are most similar?
+# ggplot(data = lca %>%
+#          filter(Food.Group %in% c("Bovine Meat (Beef Herd)",
+#                                   "Pig Meat",
+#                                   "Mutton & Goat Meat",
+#                                   "Poultry Meat")),
+#        aes(x = Region,
+#            y = Crop_m2_kg_protein,
+#            fill = Region)) +
+#   facet_wrap(~Food.Group) +
+#   geom_bar(stat = "identity") +
+#   labs(x = NULL) +
+#   theme(legend.position = c(1,1),
+#         legend.justification = c(1,1),
+#         axis.text.x = element_text(angle = 90)) +
+#   guides(fill = guide_legend(ncol = 2))
+# ggsave("Outputs/Prelim/LCAPerRegion_crop.png",
+#        height = 20, width = 25, units = "cm")
+
+# ggplot(data = lca %>%
+#          filter(Food.Group %in% c("Bovine Meat (Beef Herd)",
+#                                   "Pig Meat",
+#                                   "Mutton & Goat Meat",
+#                                   "Poultry Meat")),
+#        aes(x = Region,
+#            y = Pasture_m2_kg_protein, 
+#            fill = Region)) + 
+#   facet_wrap(~Food.Group) + 
+#   geom_bar(stat = "identity") + 
+#   labs(x = NULL) + 
+#   theme(legend.position = c(1,0.4),
+#         legend.justification = c(1,1),
+#         axis.text.x = element_text(angle = 90)) + 
+#   guides(fill = guide_legend(ncol = 2))
+# ggsave("Outputs/Prelim/LCAPerRegion_pasture.png",
+#        height = 20, width = 25, units = "cm")
+# 
+
 # Link these to the LCA data -----
 land_grab <- land_grab %>%
-  left_join(., lca) %>%
-  mutate(extra_pasture_m = kg_extra * `Perm Past`,
-         extra_arable_fallow_m = kg_extra * (Fallow + Arable),
-         extra_total_m = kg_extra * Land_total_m_kg,
+  left_join(., select(lca,
+                      Region, 
+                      Product,
+                      Pasture_m2_kg_protein:sd_Crop_m2_kg_protein))
+
+# Assign global averages where data are missing
+global_av <- lca %>%
+  group_by(Product) %>%
+  summarise(Pasture_m2_kg_protein = median(Pasture_m2_kg_protein, na.rm = TRUE),
+            sd_Pasture_m2_kg_protein = median(sd_Pasture_m2_kg_protein, na.rm = TRUE),
+            Crop_m2_kg_protein = median(Crop_m2_kg_protein, na.rm = TRUE),
+            sd_Crop_m2_kg_protein = median(sd_Crop_m2_kg_protein, na.rm = TRUE)) %>%
+  ungroup()
+
+land_grab <- land_grab %>%
+  filter(is.na(Pasture_m2_kg_protein)) %>%
+  select(ISO3:Region) %>%
+  left_join(., global_av) %>%
+  bind_rows(., land_grab %>%
+              filter(!is.na(Pasture_m2_kg_protein)))
+
+# Get land grab into sensible units ---- 
+land_grab <- land_grab %>%
+  mutate(extra_pasture_m = kg_extra * Pasture_m2_kg_protein,
+         extra_pasture_m_sd = kg_extra * sd_Pasture_m2_kg_protein,
+         extra_crop_m = kg_extra * Crop_m2_kg_protein,
+         extra_crop_m_sd = kg_extra * sd_Crop_m2_kg_protein,
+         extra_total_m = extra_pasture_m + extra_crop_m,
+         extra_total_m_sd = extra_pasture_m_sd + extra_crop_m_sd,
          extra_pasture_km = extra_pasture_m / 1000000,
-         extra_arable_fallow_km = extra_arable_fallow_m / 1000000,
-         extra_total_km = extra_total_m / 1000000)
+         extra_pasture_km_sd = extra_pasture_m_sd / 1000000,
+         extra_crop_km = extra_crop_m / 1000000,
+         extra_crop_km_sd = extra_crop_m_sd / 1000000,
+         extra_total_km = extra_total_m / 1000000,
+         extra_total_km_sd = extra_total_m_sd / 1000000)
 
 # Total up by country -----
 land_grab_country <- land_grab %>%
   group_by(ISO3, COUNTRY) %>%
   summarise(country_extra_pasture_km = sum(extra_pasture_km, na.rm = TRUE),
-            country_extra_arable_fallow_km = sum(extra_arable_fallow_km, na.rm = TRUE),
+            country_extra_crop_km = sum(extra_crop_km, na.rm = TRUE),
             country_extra_total_km = sum(extra_total_km, na.rm = TRUE)) %>%
   ungroup() 
 
@@ -141,6 +227,8 @@ ggplot(data = plot_data,
   theme(axis.text.x = element_text(angle = 90),
         legend.position = c(.9, .9))
 
-ggsave("Outputs/Prelim/LandGrab_v1.pdf",
+ggsave("Outputs/Prelim/LandGrab_v2.pdf",
        height = 15, width = 30, units = "cm")
 
+ggsave("Outputs/Prelim/LandGrab_v2.png",
+       height = 15, width = 30, units = "cm")
